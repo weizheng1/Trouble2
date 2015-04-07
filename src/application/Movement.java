ea13b0cb35582242f3326dddf5c71f0b980cbbff
ahhughes5110 <ahhughes5110@gmail.com>
2015-04-06 21:09:11 -0400

    movement class

---
+++ src/application/Movement.java
@@ -0,0 +1,31 @@
+package application;
+
+/**
+ * This class represents the movement of each players pawn across the board
+ * @author Abigail Hughes
+ *
+ */
+
+public class Movement {
+	
+	public void getPawn ()
+	{
+		//TODO Abigail get pawn assigned to each player//
+	}
+	
+	public void getDie (){
+		//TODO Abigail get rolled die amount
+	}
+	
+	public void move (){
+		//TODO Abigail move pawn according to die amount
+	}
+	
+	public void checkPlacement (){
+		//TODO Abigial check to see if a current pawn is there
+	}
+
+}
+
+
+
