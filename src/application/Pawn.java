c61d49dad0317969d7e6c415dab1c6ff20325c96
ahhughes5110 <ahhughes5110@gmail.com>
2015-04-06 20:46:58 -0400

    pawn class

---
+++ src/application/Pawn.java
@@ -0,0 +1,20 @@
+package application;
+
+/**
+ * This class represents the various pawns used by each player
+ * @author Abigail Hughes
+ *
+ */
+
+public class Pawn {
+	/**
+	 * Create Pawns for each player
+	 */
+	
+	public void addPawn(PawnPiece piece){
+	//TODO ABigail Put Piece in data structure which will store a reference to the object so it can be retrieved
+	}
+	
+	public void adjustPawn (PawnPiece piece){
+		//TODO Abigial Adjust value positioned on board to zero (home base) if players pawn is bumped
+	}
